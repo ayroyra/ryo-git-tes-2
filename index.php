5bdf4efd356bd8e3078f0a5256e4df4b95c06005
ayroyra <ayroptr@gmail.com>
2022-08-29 09:29:17 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut esse perspiciatis laborum iste doloremque deleniti excepturi molestiae accusamus sed omnis facilis quae, tempore, quis officia quod? Voluptatibus corrupti eveniet labore?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
